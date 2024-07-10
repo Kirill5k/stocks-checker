2f54a49ef10bb4abf955b9556a45961bdaf42609
kirill5k <immotional@aol.com>
2024-07-10 13:21:04 +0100

    Errors class

---
+++ modules/core/src/main/scala/stockchecker/domain/errors.scala
@@ -0,0 +1,13 @@
+package stockchecker.domain
+
+object errors {
+  sealed trait AppError extends Throwable:
+    def message: String
+    override def getMessage: String = message
+
+  object AppError:
+    final case class Http(status: Int, message: String) extends AppError
+    final case class Json(message: String)              extends AppError
+    final case class Critical(message: String)          extends AppError
+    final case class NotFound(message: String)          extends AppError
+}
