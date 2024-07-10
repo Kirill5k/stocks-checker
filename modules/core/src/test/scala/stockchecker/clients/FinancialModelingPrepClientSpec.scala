123a0c245ef580caaecd6b4c0de3563c3900dc1a
kirill5k <immotional@aol.com>
2024-07-10 09:55:48 +0100

    Test data from FinancialModelingPrepClient

---
+++ modules/core/src/test/scala/stockchecker/clients/FinancialModelingPrepClientSpec.scala
@@ -0,0 +1,9 @@
+package stockchecker.clients
+
+import kirill5k.common.cats.test.IOWordSpec
+
+class FinancialModelingPrepClientSpec extends IOWordSpec {
+  "A FinancialModelingPrepClient" when {
+
+  }
+}
