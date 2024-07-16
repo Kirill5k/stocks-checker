03a9d6d01e6978f96f3e48da704193b38a37954f
kirill5k <immotional@aol.com>
2024-07-16 10:41:53 +0100

    Setup test file for StockRepository

---
+++ modules/core/src/test/scala/stockchecker/repositories/StockRepositorySpec.scala
@@ -0,0 +1,12 @@
+package stockchecker.repositories
+
+import mongo4cats.embedded.EmbeddedMongo
+import org.scalatest.matchers.must.Matchers
+import org.scalatest.wordspec.AsyncWordSpec
+
+class StockRepositorySpec extends AsyncWordSpec with Matchers with EmbeddedMongo{
+
+  "A StockRepository" when {
+
+  }
+}
