5de56557550429af7dd7a2a4b0878c12f00d6e87
kirill5k <immotional@aol.com>
2024-07-17 09:39:24 +0100

    Alternative StockRepository#save implementation

--- modules/core/src/main/scala/stockchecker/repositories/StockRepository.scala
+++ modules/core/src/main/scala/stockchecker/repositories/StockRepository.scala
@@ -2,6 +2,7 @@ package stockchecker.repositories
 
 import cats.Monad
 import cats.syntax.functor.*
+import cats.syntax.traverse.*
 import mongo4cats.collection.MongoCollection
 import mongo4cats.database.MongoDatabase
 import stockchecker.domain.Stock
@@ -10,15 +11,24 @@ import fs2.Stream
 
 trait StockRepository[F[_]]:
   def save(stock: Stock): F[Unit]
+  def save(stocks: List[Stock]): F[Unit]
   def streamAll: Stream[F, Stock]
 
 final private class LiveStockRepository[F[_]: Monad](
     private val collection: MongoCollection[F, StockEntity]
 ) extends StockRepository[F] {
-  
+
+  override def save(stocks: List[Stock]): F[Unit] =
+    stocks
+      .map(StockEntity.from)
+      .grouped(1024)
+      .toList
+      .traverse(collection.insertMany)
+      .void
+
   override def save(stock: Stock): F[Unit] =
     collection.insertOne(StockEntity.from(stock)).void
-    
+
   override def streamAll: Stream[F, Stock] =
     collection.find.stream.map(_.toDomain)
 }
