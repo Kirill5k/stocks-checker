ead9159dcde95767bb06b3e71ccad9c524b7c6e5
kirill5k <immotional@aol.com>
2024-07-20 16:51:13 +0100

    CompanyProfileRepository test setup

---
+++ modules/core/src/test/scala/stockschecker/repositories/CompanyProfileRepositoryTest.scala
@@ -0,0 +1,12 @@
+package stockschecker.repositories
+
+import mongo4cats.embedded.EmbeddedMongo
+import org.scalatest.matchers.must.Matchers
+import org.scalatest.wordspec.AsyncWordSpec
+
+class CompanyProfileRepositoryTest extends AsyncWordSpec with Matchers with EmbeddedMongo {
+  
+  "A CompanyProfileRepository" when {
+    
+  }
+}
