118e127e0358277cb16881125c0e57f5eaf14d81
kirill5k <immotional@aol.com>
2024-08-15 10:34:34 +0100

    Command definition

--- modules/core/src/main/scala/stockschecker/actions/Action.scala
+++ modules/core/src/main/scala/stockschecker/actions/Action.scala
@@ -1,4 +1,7 @@
 package stockschecker.actions
 
+import stockschecker.common.types.EnumType
+
+object Action extends EnumType[Action](() => Action.values)
 enum Action:
   case FetchLatestStocks
--- modules/core/src/main/scala/stockschecker/domain/Command.scala
+++ modules/core/src/main/scala/stockschecker/domain/Command.scala
@@ -1,3 +1,33 @@
 package stockschecker.domain
 
-final case class Command ()
+import io.circe.Codec
+import kirill5k.common.syntax.time.*
+import stockschecker.actions.Action
+import stockschecker.common.types.IdType
+
+import java.time.Instant
+import scala.concurrent.duration.{Duration, FiniteDuration}
+
+opaque type CommandId = String
+object CommandId extends IdType[CommandId]
+
+final case class Command(
+    id: CommandId,
+    isActive: Boolean,
+    action: Action,
+    schedule: Schedule,
+    lastExecutedAt: Option[Instant],
+    executionCount: Int,
+    maxExecutions: Option[Int]
+) derives Codec.AsObject {
+  def durationUntilNextExecution(now: Instant): FiniteDuration =
+    lastExecutedAt
+      .map(schedule.nextExecutionTime)
+      .filter(_.isAfter(now))
+      .map(now.durationBetween)
+      .getOrElse {
+        schedule match
+          case _: Schedule.Periodic => Duration.Zero
+          case _: Schedule.Cron     => now.durationBetween(schedule.nextExecutionTime(now))
+      }
+}
