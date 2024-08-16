c4044a4c964d6ea390fc3311e7b665462a9a1ffd
kirill5k <immotional@aol.com>
2024-08-16 09:25:19 +0100

    Fix tests

--- modules/core/src/test/scala/stockschecker/repositories/CommandRepositorySpec.scala
+++ modules/core/src/test/scala/stockschecker/repositories/CommandRepositorySpec.scala
@@ -4,7 +4,7 @@ import org.scalatest.wordspec.AsyncWordSpec
 
 class CommandRepositorySpec  extends RepositorySpec {
   
-  override def port: Int = 12147
+  override def port: Int = 12146
   
   "A  CommandRepository" when {
     
--- modules/core/src/test/scala/stockschecker/repositories/RepositorySpec.scala
+++ modules/core/src/test/scala/stockschecker/repositories/RepositorySpec.scala
@@ -15,7 +15,7 @@ trait RepositorySpec extends AsyncWordSpec with Matchers with EmbeddedMongo  {
   def port: Int
   
   protected def withEmbeddedMongoDatabase[A](test: MongoDatabase[IO] => IO[A]): Future[A] =
-    withRunningEmbeddedMongo(12147) {
+    withRunningEmbeddedMongo(port) {
       MongoClient
         .fromConnectionString[IO](s"mongodb://localhost:${port}")
         .evalMap(_.getDatabase("stock-checker"))
