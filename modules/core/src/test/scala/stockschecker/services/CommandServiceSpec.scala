e33e55e895bf3e39c074a4d9b96c1aa881da3168
kirill5k <immotional@aol.com>
2024-08-18 13:15:04 +0100

    Tests for CommandService#execute

--- modules/core/src/test/scala/stockschecker/services/CommandServiceSpec.scala
+++ modules/core/src/test/scala/stockschecker/services/CommandServiceSpec.scala
@@ -8,12 +8,15 @@ import stockschecker.actions.{Action, ActionDispatcher}
 import stockschecker.fixtures.*
 import stockschecker.repositories.CommandRepository
 import fs2.Stream
+import stockschecker.domain.{Command, CommandId}
 
 import scala.concurrent.duration.*
 
 class CommandServiceSpec extends IOWordSpec {
 
-  given Clock[IO] = Clock.mock(ts.plus(10.minutes))
+  val now = ts.plus(10.minutes)
+
+  given Clock[IO] = Clock.mock(now)
 
   "A CommandService" when {
     "rescheduleAll" should {
@@ -40,12 +43,67 @@ class CommandServiceSpec extends IOWordSpec {
 
         val res = for
           svc <- CommandService.make(ad, repo)
-          _ <- svc.rescheduleAll
+          _   <- svc.rescheduleAll
         yield ()
 
         res.asserting { r =>
           verify(repo).streamActive
           verify(ad).dispatch(Action.Schedule(FetchLatestStocksCommand.id, 10.minutes))
+          r mustBe ()
+        }
+      }
+    }
+
+    "execute" should {
+      "dispatch an action from command" in {
+        val (ad, repo) = mocks
+        when(repo.find(any[CommandId])).thenReturnIO(FetchLatestStocksCommand)
+        when(repo.update(any[Command])).thenReturnIO(FetchLatestStocksCommand)
+        when(ad.dispatch(any[Action])).thenReturnUnit
+
+        val res = for
+          svc <- CommandService.make(ad, repo)
+          _ <- svc.execute(FetchLatestStocksCommand.id)
+        yield ()
+
+        res.asserting { r =>
+          verify(repo).find(FetchLatestStocksCommand.id)
+          verify(ad).dispatch(FetchLatestStocksCommand.action)
+          verify(repo).update(FetchLatestStocksCommand.copy(lastExecutedAt = Some(now), executionCount = 2))
+          r mustBe ()
+        }
+      }
+
+      "not execute command when maxExecutions is equal to executionCount" in {
+        val (ad, repo) = mocks
+        when(repo.find(any[CommandId])).thenReturnIO(FetchLatestStocksCommand.copy(executionCount = 2, maxExecutions = Some(2)))
+
+        val res = for
+          svc <- CommandService.make(ad, repo)
+          _ <- svc.execute(FetchLatestStocksCommand.id)
+        yield ()
+
+        res.asserting { r =>
+          verify(repo).find(FetchLatestStocksCommand.id)
+          verifyNoInteractions(ad)
+          verifyNoMoreInteractions(repo)
+          r mustBe()
+        }
+      }
+
+      "not execute command when is it inactive" in {
+        val (ad, repo) = mocks
+        when(repo.find(any[CommandId])).thenReturnIO(FetchLatestStocksCommand.copy(isActive = false))
+
+        val res = for
+          svc <- CommandService.make(ad, repo)
+          _ <- svc.execute(FetchLatestStocksCommand.id)
+        yield ()
+
+        res.asserting { r =>
+          verify(repo).find(FetchLatestStocksCommand.id)
+          verifyNoInteractions(ad)
+          verifyNoMoreInteractions(repo)
           r mustBe()
         }
       }
