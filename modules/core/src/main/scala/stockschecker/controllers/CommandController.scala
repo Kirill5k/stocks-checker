1ab90ccc9af85973c0bb5ce6b2791c375db82041
kirill5k <immotional@aol.com>
2024-08-20 11:15:16 +0100

    Create Command endpoint

--- modules/core/src/main/scala/stockschecker/controllers/CommandController.scala
+++ modules/core/src/main/scala/stockschecker/controllers/CommandController.scala
@@ -1,8 +1,11 @@
 package stockschecker.controllers
 
 import cats.effect.Async
+import io.circe.Codec
 import org.http4s.HttpRoutes
-import stockschecker.domain.{Command, CommandId}
+import stockschecker.actions.Action
+import stockschecker.controllers.CommandController.CreateCommandResponse
+import stockschecker.domain.{Command, CommandId, CreateCommand, Schedule}
 import stockschecker.services.CommandService
 import sttp.tapir.*
 import sttp.tapir.generic.auto.SchemaDerivation
@@ -18,16 +21,27 @@ final private class CommandController[F[_]: Async](
       service.getAll
         .mapResponse(identity)
     }
+  
+  private val createCommand = CommandController.createCommandEndpoint
+    .serverLogic { req =>
+      service
+        .create(CreateCommand(req.action, req.schedule, req.maxExecutions))
+        .mapResponse(cmd => CreateCommandResponse(cmd.id))
+    }
 
   val routes: HttpRoutes[F] =
     Http4sServerInterpreter[F](Controller.serverOptions).toRoutes(
-      List(getAllCommands)
+      List(
+        getAllCommands,
+        createCommand
+      )
     )
 }
 
 object CommandController extends TapirJsonCirce with SchemaDerivation {
   given Schema[CommandId] = Schema.string
-  given Schema[Command]   = Schema.string
+  given Schema[Action]   = Schema.string
+  given Schema[Schedule]   = Schema.string
 
   private val basePath = "commands"
 
@@ -35,6 +49,22 @@ object CommandController extends TapirJsonCirce with SchemaDerivation {
     .in(basePath)
     .out(jsonBody[List[Command]])
     .description("Get all commands")
+
+  final case class CreateCommandRequest(
+      action: Action,
+      schedule: Schedule,
+      maxExecutions: Option[Int] = None
+  ) derives Codec.AsObject
+
+  final case class CreateCommandResponse(
+      id: CommandId
+  ) derives Codec.AsObject
+
+  private val createCommandEndpoint = Controller.publicEndpoint.post
+    .in(basePath)
+    .in(jsonBody[CreateCommandRequest])
+    .out(jsonBody[CreateCommandResponse])
+    .description("Create new command")
 
   def make[F[_]: Async](service: CommandService[F]): F[Controller[F]] =
     Async[F].pure(CommandController[F](service))
