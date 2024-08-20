ea0d1a65de9abd82859201998231c3bb00519faa
kirill5k <immotional@aol.com>
2024-08-20 10:39:57 +0100

    Initial command controller

--- modules/core/src/main/scala/stockschecker/controllers/Controllers.scala
+++ modules/core/src/main/scala/stockschecker/controllers/Controllers.scala
@@ -13,11 +13,12 @@ import org.http4s.server.Router
 trait Controllers[F[_]]:
   def companyProfile: Controller[F]
   def stock: Controller[F]
+  def command: Controller[F]
   def health: Controller[F]
 
   def routes(using M: Monad[F]): HttpRoutes[F] =
     Router(
-      "api" -> (companyProfile.routes <+> stock.routes),
+      "api" -> (companyProfile.routes <+> stock.routes <+> command.routes),
       ""    -> health.routes
     )
 
@@ -27,7 +28,9 @@ object Controllers:
       cp <- CompanyProfileController.make(services.companyProfile)
       s  <- StockController.make(services.stock)
       h  <- HealthController.make[F]
+      c  <- CommandController.make[F](services.command)
     yield new Controllers[F]:
       override val companyProfile: Controller[F] = cp
       override val health: Controller[F]         = h
       override val stock: Controller[F]          = s
+      override val command: Controller[F]        = c
