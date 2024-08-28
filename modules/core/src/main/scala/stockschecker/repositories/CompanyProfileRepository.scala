27d60334b6d145c5ed963d0705bc1da9e0f12d69
kirill5k <immotional@aol.com>
2024-08-28 10:50:00 +0100

    Save and update CP in the repo

--- modules/core/src/main/scala/stockschecker/repositories/CompanyProfileRepository.scala
+++ modules/core/src/main/scala/stockschecker/repositories/CompanyProfileRepository.scala
@@ -2,9 +2,10 @@ package stockschecker.repositories
 
 import cats.Monad
 import cats.syntax.functor.*
+import cats.syntax.flatMap.*
 import mongo4cats.collection.MongoCollection
 import mongo4cats.database.MongoDatabase
-import mongo4cats.operations.Filter
+import mongo4cats.operations.{Filter, Update}
 import stockschecker.domain.{CompanyProfile, Ticker}
 import stockschecker.repositories.entities.CompanyProfileEntity
 import kirill5k.common.cats.syntax.applicative.*
@@ -18,14 +19,29 @@ final private class LiveCompanyProfileRepository[F[_]: Monad](
 ) extends CompanyProfileRepository[F] {
 
   override def save(cp: CompanyProfile): F[Unit] =
-    collection.insertOne(CompanyProfileEntity.from(cp)).void
+    collection
+      .count(Filter.idEq(cp.ticker))
+      .flatMap {
+        case 0 =>
+          collection.insertOne(CompanyProfileEntity.from(cp)).void
+        case _ =>
+          collection
+            .updateOne(
+              Filter.idEq(cp.ticker),
+              Update
+                .set("averageTradedVolume", cp.averageTradedVolume)
+                .set("marketCap", cp.marketCap)
+                .currentDate("lastUpdatedAt")
+            )
+            .void
+      }
 
   override def find(ticker: Ticker): F[Option[CompanyProfile]] =
     collection.find(Filter.idEq(ticker)).first.mapOpt(_.toDomain)
 }
 
 object CompanyProfileRepository:
-  def make[F[_] : Monad](database: MongoDatabase[F]): F[CompanyProfileRepository[F]] =
+  def make[F[_]: Monad](database: MongoDatabase[F]): F[CompanyProfileRepository[F]] =
     database
       .getCollectionWithCodec[CompanyProfileEntity]("company-profiles")
       .map(LiveCompanyProfileRepository[F](_))
