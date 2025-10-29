e46d70edec55a965194009c0752ec748963fb13a
kirill5k <immotional@aol.com>
2025-10-29 13:14:52 +0000

    Add missing actions in ActionExecutor

--- modules/core/src/main/scala/stockschecker/actions/ActionExecutor.scala
+++ modules/core/src/main/scala/stockschecker/actions/ActionExecutor.scala
@@ -34,6 +34,9 @@ final private class LiveActionExecutor[F[_]](
           services.companyProfile.fetchLatest(ticker)
         case Action.FetchLatestPricePerformanceSummary(ticker) =>
           services.price.fetchLatestPerformanceSummary(ticker)
+        case Action.DiscoverSecurities(_, _) => ???
+        case Action.EnrichCompanyProfiles(_) => ???
+        case Action.FetchPricePerformanceSummaries(_, _, _) => ???
       ).handleErrorWith {
         case error: AppError =>
           logger.warn(error)(s"Domain error while processing action $action")
