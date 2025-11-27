ab7d999f23bc742018059fa36b65670c6c69dbaa
kirill5k <immotional@aol.com>
2025-11-27 09:28:01 +0000

    Define StockView

--- modules/core/src/main/scala/stockschecker/controllers/StockController.scala
+++ modules/core/src/main/scala/stockschecker/controllers/StockController.scala
@@ -1,14 +1,18 @@
 package stockschecker.controllers
 
 import cats.effect.kernel.Async
+import io.circe.Codec as CirceCodec
 import org.http4s.HttpRoutes
 import stockschecker.common.config.ApiConfig
-import stockschecker.domain.{Stock, Ticker}
+import stockschecker.controllers.StockController.StockView
+import stockschecker.domain.{Exchange, SecurityKind, Stock, Ticker}
 import stockschecker.services.StockService
 import sttp.tapir.*
 import sttp.tapir.generic.auto.SchemaDerivation
 import sttp.tapir.json.circe.TapirJsonCirce
 import sttp.tapir.server.http4s.Http4sServerInterpreter
+
+import java.time.{Instant, LocalDate}
 
 final private class StockController[F[_]: Async](
     private val stockService: StockService[F],
@@ -17,7 +21,7 @@ final private class StockController[F[_]: Async](
 
   private val getStockByTicker = secured(StockController.getStockByTickerEndpoint)
     .serverLogic { _ => ticker =>
-      stockService.findByTicker(ticker).asResponse
+      stockService.findByTicker(ticker).mapToResponse(StockView.from)
     }
 
   override val routes: HttpRoutes[F] =
@@ -26,9 +30,50 @@ final private class StockController[F[_]: Async](
 
 object StockController extends TapirJsonCirce with SchemaDerivation {
 
+  final private case class StockView(
+      ticker: Ticker,
+      name: String,
+      security: StockSecurityView,
+      profile: Option[StockCompanyProfileView],
+      performanceSummary: Option[StockPricePerformanceSummaryView]
+  ) derives CirceCodec.AsObject
+
+  private object StockView:
+    def from(stock: Stock): StockView = ???
+
+  final private case class StockSecurityView(
+      exchange: Exchange,
+      kind: SecurityKind
+  ) derives CirceCodec.AsObject
+
+  final private case class StockCompanyProfileView(
+      country: String,
+      industry: String,
+      description: Option[String],
+      website: String,
+      ipoDate: LocalDate,
+      currency: String,
+      marketCap: Long,
+      lastUpdatedAt: Option[Instant]
+  ) derives CirceCodec.AsObject
+
+  final private case class StockPricePerformanceSummaryView(
+      latestPrice: BigDecimal,
+      latestPriceDate: LocalDate,
+      oneMonthChange: Option[BigDecimal],
+      threeMonthChange: Option[BigDecimal],
+      sixMonthChange: Option[BigDecimal],
+      oneYearChange: Option[BigDecimal],
+      threeYearChange: Option[BigDecimal],
+      fiveYearChange: Option[BigDecimal],
+      tenYearChange: Option[BigDecimal],
+      maxChange: Option[BigDecimal],
+      lastUpdatedAt: Option[Instant]
+  ) derives CirceCodec.AsObject
+
   private val getStockByTickerEndpoint = Controller.secureEndpoint.get
     .in("stocks" / path[Ticker]("ticker"))
-    .out(jsonBody[Stock])
+    .out(jsonBody[StockView])
 
   def make[F[_]: Async](stockService: StockService[F], config: ApiConfig): F[Controller[F]] =
     Async[F].pure(StockController[F](stockService, ApiKeyRequirement.Required(config.key)))
