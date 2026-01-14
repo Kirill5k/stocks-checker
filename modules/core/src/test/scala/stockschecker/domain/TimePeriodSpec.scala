5dddf86446cf615acba5e0cf344112b406e92962
kirill5k <immotional@aol.com>
2026-01-14 11:38:01 +0000

    Refactor TimePeriodSpec

--- modules/core/src/test/scala/stockschecker/domain/TimePeriodSpec.scala
+++ modules/core/src/test/scala/stockschecker/domain/TimePeriodSpec.scala
@@ -5,21 +5,25 @@ import io.circe.syntax.*
 import org.scalatest.EitherValues
 import org.scalatest.matchers.must.Matchers
 import org.scalatest.wordspec.AnyWordSpec
+import org.scalatest.prop.TableDrivenPropertyChecks
 
-class TimePeriodSpec extends AnyWordSpec with Matchers with EitherValues {
+class TimePeriodSpec extends AnyWordSpec with Matchers with EitherValues with TableDrivenPropertyChecks {
 
   "TimePeriod" should {
     "decode and encode into json" in {
-      Map(
-        TimePeriod.OneMonth   -> "one-month",
-        TimePeriod.ThreeMonth -> "three-month",
-        TimePeriod.SixMonth   -> "six-month",
-        TimePeriod.OneYear    -> "one-year",
-        TimePeriod.ThreeYear  -> "three-year",
-        TimePeriod.FiveYear   -> "five-year",
-        TimePeriod.TenYear    -> "ten-year",
-        TimePeriod.Max        -> "max"
-      ).foreach { case (period, json) =>
+      val testCases = Table(
+        ("period", "json"),
+        (TimePeriod.OneMonth, "one-month"),
+        (TimePeriod.ThreeMonth, "three-month"),
+        (TimePeriod.SixMonth, "six-month"),
+        (TimePeriod.OneYear, "one-year"),
+        (TimePeriod.ThreeYear, "three-year"),
+        (TimePeriod.FiveYear, "five-year"),
+        (TimePeriod.TenYear, "ten-year"),
+        (TimePeriod.Max, "max")
+      )
+
+      forAll(testCases) { (period, json) =>
         period.asJson.noSpaces mustBe s""""$json""""
         decode[TimePeriod](s""""$json"""") mustBe Right(period)
       }
