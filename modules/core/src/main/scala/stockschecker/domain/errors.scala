47cb5c71d5d3b4ab2f23b740a77f0f3d83beeba7
kirill5k <immotional@aol.com>
2026-03-11 14:20:57 +0000

    Fix typo in error message

--- modules/core/src/main/scala/stockschecker/domain/errors.scala
+++ modules/core/src/main/scala/stockschecker/domain/errors.scala
@@ -33,7 +33,7 @@ object errors {
       override val message: String = s"Could not find price analytics for $ticker"
 
     final case class FinancialMetricsNotFound(ticker: Ticker) extends NotFound:
-      override val message: String = s"Could not find financial metrids for $ticker"
+      override val message: String = s"Could not find financial metrics for $ticker"
 
     final case class JsonParsingFailure(original: String, error: String) extends AppError:
       override val message: String = s"Failed to parse json response: $error\n$original"
