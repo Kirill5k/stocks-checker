b33d89978d9861b4ecf88fe16e1dfe87faf6c5e2
kirill5k <immotional@aol.com>
2026-03-12 12:22:39 +0000

    Support for additional filtering query params in stocks endpoint

--- modules/core/src/main/scala/stockschecker/controllers/StockController.scala
+++ modules/core/src/main/scala/stockschecker/controllers/StockController.scala
@@ -5,7 +5,7 @@ import io.circe.Codec as CirceCodec
 import org.http4s.HttpRoutes
 import stockschecker.common.config.ApiConfig
 import stockschecker.controllers.StockController.{StockQueryParams, StockView}
-import stockschecker.domain.{Exchange, SecurityKind, Stock, StockAnalysisMetrics, StockAnalysisScores, StockFilters, StockSortField, Ticker}
+import stockschecker.domain.{Exchange, SecurityKind, Stock, StockAnalysisMetrics, StockAnalysisScores, StockFilters, StockSortField, Ticker, TimePeriod}
 import stockschecker.services.StockService
 import sttp.model.StatusCode
 import sttp.tapir.*
@@ -28,28 +28,36 @@ final private class StockController[F[_]: Async](
   private val getAllStocks = secured(StockController.getAllStocksEndpoint)
     .serverLogic { _ => (params: StockQueryParams) =>
       val filters = StockFilters(
-        exchange = params.exchange,
-        kind = params.kind,
-        isActive = params.isActive,
-        country = params.country,
-        minMarketCap = params.minMarketCap,
-        maxMarketCap = params.maxMarketCap,
-        minPrice = params.minPrice,
-        maxPrice = params.maxPrice,
-        minOverallScore = params.minOverallScore,
-        minCagrScore = params.minCagrScore,
-        minVolatilityScore = params.minVolatilityScore,
-        minPE = params.minPE,
-        maxPE = params.maxPE,
-        minROE = params.minROE,
-        maxDebtToEquity = params.maxDebtToEquity,
-        minProfitMargin = params.minProfitMargin,
-        minFreeCashFlow = params.minFreeCashFlow,
-        minRevenueGrowth5Y = params.minRevenueGrowth5Y,
-        minEpsGrowth5Y = params.minEpsGrowth5Y,
-        sortBy = params.sortBy
+        exchange            = params.basic.exchange,
+        kind                = params.basic.kind,
+        isActive            = params.basic.isActive,
+        country             = params.basic.country,
+        minMarketCap        = params.market.minMarketCap,
+        maxMarketCap        = params.market.maxMarketCap,
+        minPrice            = params.market.minPrice,
+        maxPrice            = params.market.maxPrice,
+        minChange           = params.market.minChange,
+        maxChange           = params.market.maxChange,
+        period              = params.market.period,
+        minOverallScore     = params.scores.minOverallScore,
+        minCagrScore        = params.scores.minCagrScore,
+        minVolatilityScore  = params.scores.minVolatilityScore,
+        maxDrawdown         = params.scores.maxDrawdown,
+        minConsistencyScore = params.scores.minConsistencyScore,
+        minPE               = params.financial.minPE,
+        maxPE               = params.financial.maxPE,
+        minROE              = params.financial.minROE,
+        maxDebtToEquity     = params.financial.maxDebtToEquity,
+        minProfitMargin     = params.financial.minProfitMargin,
+        maxProfitMargin     = params.financial.maxProfitMargin,
+        minFreeCashFlow     = params.financial.minFreeCashFlow,
+        minDividendYield    = params.financial.minDividendYield,
+        maxDividendYield    = params.financial.maxDividendYield,
+        minRevenueGrowth5Y  = params.financial.minRevenueGrowth5Y,
+        minEpsGrowth5Y      = params.financial.minEpsGrowth5Y,
+        sortBy              = params.basic.sortBy
       )
-      stockService.findAll(filters, params.limit).mapToResponse(_.map(StockView.from))
+      stockService.findAll(filters, params.basic.limit).mapToResponse(_.map(StockView.from))
     }
 
   private val deactivateStock = secured(StockController.deactivateStockEndpoint)
@@ -63,29 +71,57 @@ final private class StockController[F[_]: Async](
 
 object StockController extends TapirJsonCirce with SchemaDerivation {
 
-  final case class StockQueryParams(
+  // --- Query parameter groups (split to work around Tapir's 22-param limit) ---
+
+  final case class BasicFilters(
       limit: Option[Int],
       exchange: Option[Exchange],
       kind: Option[SecurityKind],
       isActive: Option[Boolean],
       country: Option[String],
+      sortBy: Option[StockSortField]
+  )
+
+  final case class MarketFilters(
       minMarketCap: Option[Long],
       maxMarketCap: Option[Long],
       minPrice: Option[BigDecimal],
       maxPrice: Option[BigDecimal],
+      minChange: Option[BigDecimal],
+      maxChange: Option[BigDecimal],
+      period: Option[TimePeriod]
+  )
+
+  final case class ScoreFilters(
       minOverallScore: Option[BigDecimal],
       minCagrScore: Option[BigDecimal],
       minVolatilityScore: Option[BigDecimal],
+      maxDrawdown: Option[BigDecimal],
+      minConsistencyScore: Option[BigDecimal]
+  )
+
+  final case class FinancialFilters(
       minPE: Option[BigDecimal],
       maxPE: Option[BigDecimal],
       minROE: Option[BigDecimal],
       maxDebtToEquity: Option[BigDecimal],
       minProfitMargin: Option[BigDecimal],
+      maxProfitMargin: Option[BigDecimal],
       minFreeCashFlow: Option[BigDecimal],
+      minDividendYield: Option[BigDecimal],
+      maxDividendYield: Option[BigDecimal],
       minRevenueGrowth5Y: Option[BigDecimal],
-      minEpsGrowth5Y: Option[BigDecimal],
-      sortBy: Option[StockSortField]
+      minEpsGrowth5Y: Option[BigDecimal]
   )
+
+  final case class StockQueryParams(
+      basic: BasicFilters,
+      market: MarketFilters,
+      scores: ScoreFilters,
+      financial: FinancialFilters
+  )
+
+  // --- Response views ---
 
   final private case class FinancialMetricsView(
       peRatioTtm: Option[BigDecimal],
@@ -207,34 +243,58 @@ object StockController extends TapirJsonCirce with SchemaDerivation {
   ) derives CirceCodec.AsObject
 
   private val stockByTickerPath = "stocks" / path[Ticker]("ticker")
-  
+
+  private val basicInput =
+    query[Option[Int]]("limit")
+      .and(query[Option[Exchange]]("exchange"))
+      .and(query[Option[SecurityKind]]("kind"))
+      .and(query[Option[Boolean]]("isActive"))
+      .and(query[Option[String]]("country"))
+      .and(query[Option[StockSortField]]("sortBy"))
+      .mapTo[BasicFilters]
+
+  private val marketInput =
+    query[Option[Long]]("minMarketCap")
+      .and(query[Option[Long]]("maxMarketCap"))
+      .and(query[Option[BigDecimal]]("minPrice"))
+      .and(query[Option[BigDecimal]]("maxPrice"))
+      .and(query[Option[BigDecimal]]("minChange"))
+      .and(query[Option[BigDecimal]]("maxChange"))
+      .and(query[Option[TimePeriod]]("period"))
+      .mapTo[MarketFilters]
+
+  private val scoreInput =
+    query[Option[BigDecimal]]("minOverallScore")
+      .and(query[Option[BigDecimal]]("minCagrScore"))
+      .and(query[Option[BigDecimal]]("minVolatilityScore"))
+      .and(query[Option[BigDecimal]]("maxDrawdown"))
+      .and(query[Option[BigDecimal]]("minConsistencyScore"))
+      .mapTo[ScoreFilters]
+
+  private val financialInput =
+    query[Option[BigDecimal]]("minPE")
+      .and(query[Option[BigDecimal]]("maxPE"))
+      .and(query[Option[BigDecimal]]("minROE"))
+      .and(query[Option[BigDecimal]]("maxDebtToEquity"))
+      .and(query[Option[BigDecimal]]("minProfitMargin"))
+      .and(query[Option[BigDecimal]]("maxProfitMargin"))
+      .and(query[Option[BigDecimal]]("minFreeCashFlow"))
+      .and(query[Option[BigDecimal]]("minDividendYield"))
+      .and(query[Option[BigDecimal]]("maxDividendYield"))
+      .and(query[Option[BigDecimal]]("minRevenueGrowth5Y"))
+      .and(query[Option[BigDecimal]]("minEpsGrowth5Y"))
+      .mapTo[FinancialFilters]
+
   private val getStockByTickerEndpoint = Controller.secureEndpoint.get
     .in(stockByTickerPath)
     .out(jsonBody[StockView])
 
   private val getAllStocksEndpoint = Controller.secureEndpoint.get
     .in("stocks")
-    .in(query[Option[Int]]("limit"))
-    .in(query[Option[Exchange]]("exchange"))
-    .in(query[Option[SecurityKind]]("kind"))
-    .in(query[Option[Boolean]]("isActive"))
-    .in(query[Option[String]]("country"))
-    .in(query[Option[Long]]("minMarketCap"))
-    .in(query[Option[Long]]("maxMarketCap"))
-    .in(query[Option[BigDecimal]]("minPrice"))
-    .in(query[Option[BigDecimal]]("maxPrice"))
-    .in(query[Option[BigDecimal]]("minOverallScore"))
-    .in(query[Option[BigDecimal]]("minCagrScore"))
-    .in(query[Option[BigDecimal]]("minVolatilityScore"))
-    .in(query[Option[BigDecimal]]("minPE"))
-    .in(query[Option[BigDecimal]]("maxPE"))
-    .in(query[Option[BigDecimal]]("minROE"))
-    .in(query[Option[BigDecimal]]("maxDebtToEquity"))
-    .in(query[Option[BigDecimal]]("minProfitMargin"))
-    .in(query[Option[BigDecimal]]("minFreeCashFlow"))
-    .in(query[Option[BigDecimal]]("minRevenueGrowth5Y"))
-    .in(query[Option[BigDecimal]]("minEpsGrowth5Y"))
-    .in(query[Option[StockSortField]]("sortBy"))
+    .in(basicInput)
+    .in(marketInput)
+    .in(scoreInput)
+    .in(financialInput)
     .mapInTo[StockQueryParams]
     .out(jsonBody[List[StockView]])
 
